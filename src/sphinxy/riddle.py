6adb00ac775383188eceb7ca8b9b10034d1a7156
Justin Quick <jquick@stsci.edu>
2023-04-20 14:13:10 -0600

    docstrs

--- src/sphinxy/riddle.py
+++ src/sphinxy/riddle.py
@@ -8,7 +8,22 @@ class Riddle:
     answer: str
 
     def check_answer(self, answer: str) -> bool:
+        """
+        The function checks if a given answer matches a stored answer, ignoring case sensitivity.
+
+        Args:
+          answer (str): The answer parameter is a string that represents the user's answer to a
+        question.
+
+        Returns:
+          The function `check_answer` takes in a string `answer` and returns a boolean value indicating
+        whether the lowercase version of `answer` is equal to the lowercase version of the answer stored
+        in the object's `answer` attribute.
+        """
         return answer.lower() == self.answer.lower()
 
     def get_hint(self) -> Iterator[str]:
+        """
+        This function returns an iterator that yields each character in a given answer.
+        """
         yield from iter(self.answer)
--- src/sphinxy/sphinx.py
+++ src/sphinxy/sphinx.py
@@ -17,16 +17,46 @@ class Sphinx:
         )
 
     def introduce(self) -> str:
+        """
+        The function returns a string introducing a mythical character named after the value of the
+        `_name` attribute.
+
+        Returns:
+          A string that introduces the speaker as a guardian of Thebes who has posed riddles to those
+        who approach them. The string includes the speaker's name, which is stored in the `_name`
+        attribute.
+        """
         return (
             f"Greetings, mortals. I am {self._name}. I have guarded the city of Thebes"
             "for centuries and posed riddles to those who dared to approach me."
         )
 
     def update_riddle(self, riddle: Riddle) -> str:
+        """
+        This function updates the riddle and returns a message asking if the user is ready to solve it.
+
+        Args:
+          riddle (Riddle): Riddle object that contains the updated riddle information.
+
+        Returns:
+          a string that says "I have updated my riddle. Are you ready to solve it?"
+        """
         self._riddle = riddle
         return "I have updated my riddle. Are you ready to solve it?"
 
     def pose_riddle(self, include_hint: bool = False) -> tuple[str, str | None]:
+        """
+        This Python function poses a riddle and optionally includes a hint.
+
+        Args:
+          include_hint (bool): include_hint is a boolean parameter that determines whether or not to
+        include a hint in the returned tuple. If it is set to True, a hint will be included in the
+        tuple. If it is set to False, the hint will be None. Defaults to False
+
+        Returns:
+          A tuple containing a string (the riddle question) and either a string (the hint) or None,
+        depending on the value of the include_hint parameter.
+        """
         hint = (
             f"Hint: The answer starts with the letter '{self._riddle.get_hint()}'."
             if include_hint
@@ -35,6 +65,23 @@ class Sphinx:
         return (self._riddle.question, hint)
 
     def check_riddle_answer(self, answer: str, return_hint: bool = False) -> str:
+        """
+        This function checks if a riddle answer is correct and returns a message with a hint if the
+        answer is wrong and the hint flag is set to True, otherwise it raises an exception.
+
+        Args:
+          answer (str): The answer provided by the user to the riddle question.
+          return_hint (bool): return_hint is a boolean parameter that determines whether or not to
+        return a hint if the answer is incorrect. If set to True, the function will return a hint along
+        with the message "Your answer was wrong." If set to False, the function will raise an exception
+        with the message "Your answer was. Defaults to False
+
+        Returns:
+          a string. If the answer is correct, it returns "Your answer was correct. You may pass." If the
+        answer is wrong and the parameter return_hint is True, it returns a hint about the answer.
+        Otherwise, it raises an IncorrectAnswer exception with the message "Your answer was wrong. You
+        shall not pass."
+        """
         if self._riddle.check_answer(answer):
             return "Your answer was correct. You may pass."
         elif return_hint:
